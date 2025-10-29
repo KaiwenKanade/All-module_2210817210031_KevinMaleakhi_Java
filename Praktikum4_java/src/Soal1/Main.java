b665298cd29efac61c2091739b4cd0e358466282
kaiwen <115568456+KaiwenKanade@users.noreply.github.com>
2025-10-29 21:58:17 +0800

    Add files via upload

---
+++ Praktikum4_java/src/Soal1/Main.java
@@ -0,0 +1,5 @@
+package Soal1; public class Main {
+    public static void main (String []Args) { HewanPeliharaan	Kucing1	=	new
+            HewanPeliharaan (null, null); Kucing1.display();
+    }
+}
