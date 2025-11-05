2a9282645432c03eb8dc56bc3d1661a7ec7e921b
kaiwen <115568456+KaiwenKanade@users.noreply.github.com>
2025-11-05 21:14:15 +0800

    Add files via upload

---
+++ Praktikum05_java/src/soal1/Paint.java
@@ -0,0 +1,16 @@
+package soal1;
+
+public class Paint {
+    private double coverage; // feet per gallon
+
+    public Paint(double c) {
+        coverage = c;
+    }
+
+    public double amount(Shape s) {
+        System.out.println("Computing amount for " + s);
+        double amountOfPaint = s.area() / coverage;
+        return amountOfPaint;
+    }
+}
+
---
+++ Praktikum05_java/src/soal1/Shape.java
@@ -0,0 +1,7 @@
+package soal1;
+public abstract class Shape { protected String shapeName; public Shape(String shapeName) { this.shapeName=shapeName;
+}
+    protected abstract double area(); public String toString(){
+        return shapeName;
+    }
+}
