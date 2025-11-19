2b29006841c01a3112a95726a8c0f140768e5e87
kaiwen <115568456+KaiwenKanade@users.noreply.github.com>
2025-11-19 21:27:45 +0800

    Delete PRAKTIKUM6 directory

--- PRAKTIKUM6/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.praktikum6 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.praktikum6 to javafx.fxml;
-    exports com.example.praktikum6;
-    exports PRAKTIKUM6;
-}
